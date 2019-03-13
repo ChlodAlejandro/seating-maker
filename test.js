d6d884242b5e5b28a08958d4fbfa778e5bd635e2
Chlod Alejandro <chlodaidanalejandro@hotmail.com>
2019-03-13 23:08:06 +0800

    added worstCompat

--- test.js
+++ test.js
@@ -25,11 +25,18 @@ for (var c = 0; c < studentIDs.length; c++) {
 
 console.log("=== [COMPATIBILITIES] ===");
 
+var bestStudents;
+var bestStudentsScore = 0;
+var worstStudents;
+var worstStudentsScore = 1;
+
 for (var s = 0; s < randomStudents.length; s++) {
 	console.log("Compatibilities for student: " + randomStudents[s].id);
 	let student = randomStudents[s];
 	var bestStudent;
 	var bestStudentScore = 0;
+	var worstStudent;
+	var worstStudentScore = 1;
 	for (var s2 = 0; s2 < randomStudents.length; s2++) {
 		if (s !== s2) {
 			let student2 = randomStudents[s2];
@@ -39,9 +46,27 @@ for (var s = 0; s < randomStudents.length; s++) {
 				bestStudent = student2.id;
 				bestStudentScore = compat;
 			}
+			if (bestStudentsScore < compat) {
+				bestStudents = `${student.id} => ${student2.id}`;
+				bestStudentsScore = compat;
+			}
+			if (worstStudentScore > compat) {
+				worstStudent = student2.id;
+				worstStudentScore = compat;
+			}
+			if (worstStudentsScore > compat) {
+				worstStudents = `${student.id} => ${student2.id}`;
+				worstStudentsScore = compat;
+			}
 		}
 	}
 	console.log("");
 	console.log(`Best compatible with: \"${bestStudent}\" (${bestStudentScore})`);
+	console.log(`Worst compatible with: \"${worstStudent}\" (${worstStudentScore})`);
 	console.log("");
 }
+
+console.log("");
+console.log(`Best compatibilities: \"${bestStudents}\" (${bestStudentsScore})`);
+console.log(`Worst compatibilities: \"${worstStudents}\" (${worstStudentsScore})`);
+console.log("");
